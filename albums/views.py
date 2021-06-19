fe784f3ca232e54c3f901a61c54607e437f368eb
qbarefoot <quintenbarefoot@gmail.com>
2021-06-19 14:32:20 -0400

    added albums, making progress

---
+++ albums/views.py
@@ -0,0 +1,9 @@
+from albums.models import Album
+from django.shortcuts import render
+from .models import Album
+
+# Create your views here.
+def list_albums(request):
+    albums = Album.objects.all()
+    return render(request, "albums/list_albums.html",
+                  {"albums": albums})
